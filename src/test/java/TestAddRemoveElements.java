f14631d3cd7b90e03deb802f8e5a7fdcdb87a6ba
Jonathan Wheeler <j.wheeler27@gmail.com>
2021-05-24 08:33:46 -0700

    Start test for add/remove elements

---
+++ src/test/java/TestAddRemoveElements.java
@@ -0,0 +1,32 @@
+package com.wheeler;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.http.client.*;
+
+import java.util.List;
+
+public class TestAddRemoveElements {
+
+    private static ChromeDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
+
+
+}
